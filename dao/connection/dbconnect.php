c7bb6ad456a7c0e690704706b05b676e2076b8b5
Sebuuma Daniel <sedzsoft@gmail.com>
2014-09-15 20:55:56 +0300

    Adding MySQL DB settings

--- dao/connection/dbconnect.php
+++ dao/connection/dbconnect.php
@@ -1,4 +1,15 @@
 <?php
-$conn = mysql_connect("localhost","root","root");
-$db = mysql_select_db("bugtracker",$conn);
+if($_SERVER['SERVER_NAME'] == 'localhost'){
+    $conn = mysql_connect("localhost","root","root");
+    $db = mysql_select_db("bugtracker",$conn);
+}else{
+    $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"],1);
+    mysqli_connect($server, $username, $password);
+    mysqli_select_db($db);
+}
+
 ?>
