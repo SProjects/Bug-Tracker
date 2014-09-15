d1c42cec39b4f31e6389740723d4f9bd8fa71a60
Sebuuma Daniel <sedzsoft@gmail.com>
2014-09-15 17:52:08 +0300

    Tests to determine if 2 Bugs are equal or not.

--- tests/models/BugTest.php
+++ tests/models/BugTest.php
@@ -8,7 +8,7 @@ class BugTest extends PHPUnit_Framework_TestCase{
     private $bug;
 
     public function setUp(){
-        $this->bug = new Bug("Bug title", "Bug description", new User("username", "password"), new Status(null, "OPEN", 1));
+        $this->bug = new Bug("Bug title", "Bug description", new User("username", "password", null, null), new Status(null, "OPEN", 1), null);
     }
 
     public function tearDown(){
@@ -32,4 +32,13 @@ class BugTest extends PHPUnit_Framework_TestCase{
         $this->assertNotEquals("Bug descriptions", $this->bug->getDescription());
     }
 
+    public function testTwoBugsAreEqual(){
+        $newBug = new Bug("Bug title", "Bug description", new User("username", "password", null, null), new Status(null, "OPEN", 1), null);
+        $this->assertEquals(TRUE, $newBug->equals($this->bug));
+    }
+
+    public function testTwoBugsAreNotEqual(){
+        $newBug = new Bug("Bug title1", "Bug description1", new User("username1", "password", null, null), new Status(null, "OPEN", 1), null);
+        $this->assertNotEquals(TRUE, $newBug->equals($this->bug));
+    }
 }
